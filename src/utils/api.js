1f2817fd2fd681ccbb8d8b52be4a60b8fa1ed216
Oliver Munala <munalaoliverngoitsi@gmail.com>
2018-11-19 23:01:11 +0300

    fix(api): change graphql url

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,8 +7,7 @@ const instance = axios.create();
 instance.defaults.headers.common['Content-Type'] = 'application/json';
 instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-// const graphqlUrl = `${process.env.REACT_APP_API_HOST}/api/graphql`;
-const graphqlUrl = 'http://localhost:3002/api/graphql';
+const graphqlUrl = `${process.env.REACT_APP_API_HOST}/api/graphql`;
 
 export const handleError = (error) => {
   if (error.response && error.response.data.message) {
